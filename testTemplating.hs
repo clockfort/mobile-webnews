f2d2b0ed626d880a0f2badcbc185a2766342cdff
Chris Lockfort <clockfort@csh.rit.edu>
2012-08-25 18:38:58 -0400

    organizational changes leading up to serving content via scotty (WAI/WARP)

--- testTemplating.hs
+++ testTemplating.hs
@@ -2,10 +2,14 @@ import Data.Maybe
 import Text.Html
 import Text.StringTemplate
 
-main = do newsgroupsFile <- readFile "newsgroups.template.html"
-	  headerFile <- readFile "header.template.html"
-	  let newsgroups = newSTMP newsgroupsFile :: StringTemplate String
-	  let header = newSTMP headerFile :: StringTemplate String
-	  let group = groupStringTemplates [("newsgroups", newsgroups),("header", header)]
-	  --setEncoderGroup stringToHtmlString group 
-	  putStrLn $ toString $ fromJust $ getStringTemplate "newsgroups" group
+main = renderNewsgroupIndex
+
+renderNewsgroupIndex = do
+	newsgroupsFile <- readFile "templates/newsgroups.template.html"
+	headerFile <- readFile "templates/header.template.html"
+	let newsgroups = newSTMP newsgroupsFile :: StringTemplate String
+	let header = newSTMP headerFile :: StringTemplate String
+	let group = groupStringTemplates [("newsgroups", newsgroups),("header", header)]
+	--setEncoderGroup stringToHtmlString group 
+	putStrLn $ toString $ fromJust $ getStringTemplate "newsgroups" group
+
