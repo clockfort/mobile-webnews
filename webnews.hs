d4f7f6ead00809291b83e94ca7e6f86e601d7ded
Chris Lockfort <clockfort@csh.rit.edu>
2012-08-25 20:20:03 -0400

    hackage is down, so I'm not sure if these compile. Probably will though.

--- webnews.hs
+++ webnews.hs
@@ -1,3 +1,31 @@
-import WebNewsAPI
+-- For Scotty
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
 
-main = get "user" "API_KEY_GOES_HERE"
+-- For Me
+import Control.Monad.Trans
+import Data.Text.Lazy
+
+-- Project-Internal
+-- TODO: Hookup internal modules
+
+import Network.Wai.Handler.Warp
+
+waiSettings = defaultSettings { settingsPort = (fromInteger listenPort), settingsHost = HostIPv4 }
+opts = Options {verbose = 1, settings = waiSettings}
+
+main = scottyOpts opts $ do
+	get "/" $ text "200: OK, Content not written yet"
+	
+--	TODO: Actually write these functions. This is just brainstorming.	
+--	get "/newsgroups/:newsgroup" $ do
+--		newsgroup <- param "newsgroup"
+--		posts <- liftIO $ getPostsFromNewsgroup $ unpack newsgroup
+--		html $ pack $ renderNewsgroupPage newsgroup posts
+		
+	get "/content/:handle" $ do
+		handle <- param "handle"
+		file $ "content/" ++ unpack handle
+	get "/themes/:handle" $ do
+		handle <- param "handle"
+		file $ "themes/" ++ unpack handle
