f092c74810af4b5471b8867da14814f0f716e1e5
Chris Lockfort <clockfort@redhat.com>
2012-08-31 04:14:45 -0400

    API works! Just need to add function coverage. Also added import parameterization for future sanity

--- WebNewsAPI.hs
+++ WebNewsAPI.hs
@@ -7,9 +7,10 @@ module WebNewsAPI where
 	import Network.HTTP
 	import Data.Aeson (decode, encode)
 	import Data.Aeson.TH (deriveJSON)
-	import qualified Data.ByteString.Lazy.Char8 as BL
+	import qualified Data.ByteString.Lazy.Char8 as BL (pack, unpack)
 	import WebNewsAPITypes
-	import Data.Maybe
+	import Data.Maybe (Maybe, fromJust)
+	import Control.Monad.Trans (liftIO)
 
 	opts = [CurlHttpHeaders ["Accept: application/json"]]
 	baseURL = "https://webnews-dev.csh.rit.edu/" --Temporary, production environment doesn't have API access yet
@@ -20,18 +21,19 @@ module WebNewsAPI where
 	parameterize queryList = '?' : join "&" ( map (\ x -> concat[fst x, "=", urlEncode (snd x)]) queryList )
 	
 	$(deriveJSON id ''Preferences)
-	$(deriveJSON id ''User)
+--	$(deriveJSON id ''User)
 	$(deriveJSON id ''Unread)
 	$(deriveJSON id ''UnreadCounts)
 
-	get url apiKey = withCurlDo $ do
+	getJSON url apiKey = withCurlDo $ do
 		curl <- initialize
 		setopts curl opts
 		response <- do_curl_ curl (loginEncapsulation url apiKey) method_GET :: IO CurlResponse
-		let json =  respBody response
-		putStrLn json
-		let req = (decode (BL.pack json)) :: Maybe Unread
-		putStrLn $ show (fromJust req)
-		let expected = Unread $ UnreadCounts 36 0 0
-		putStrLn $ BL.unpack $ encode expected
-		putStrLn "after"
+		let json = respBody response
+		return json
+
+	getUnread apiKey = do
+		json <- getJSON "unread_counts" apiKey
+		let req = (decode (BL.pack (json))) :: Maybe Unread
+		return req
+	
